bac015cba5c02c8efa73fd29e630d8102757e9d7
victor <894609987@qq.com>
2020-09-21 17:38:23 +0800

    change

---
+++ day17/src/com/hangzhou/victor/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.hangzhou.victor;
+
+/**
+ * @Description:
+ * @Author: victor
+ * @Date: 2020/9/21  17:37
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
