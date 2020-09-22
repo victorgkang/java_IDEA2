af16b93121ebe2b5e0dd2b19493dd70b34862396
victor <894609987@qq.com>
2020-09-22 21:54:23 +0800

    change

---
+++ day18/src/com/hangzhou/victor/TestInterface.java
@@ -0,0 +1,30 @@
+package com.hangzhou.victor;
+
+/**
+ * @Description:
+ * @Author: victor
+ * @Date: 2020/9/22  21:48
+ */
+public class TestInterface {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+interface IA{
+    void method1();
+    void method2();
+}
+
+abstract class A implements IA{    //如何不覆盖接口的方法，则该类还是抽象类
+    public void method1(){
+
+    }
+}
+
+class B extends A{               //继承父类
+    public void method2(){
+
+    }
+}
